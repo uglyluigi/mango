827608bb898a794edfc296dbc50f1deb2131a3b8
Brennan Forrest <5006946+uglyluigi@users.noreply.github.com>
2023-02-18 00:02:37 -0600

    Cool

--- src/components/BackButton.vue
+++ src/components/BackButton.vue
@@ -1,10 +1,15 @@
 <template>
-  <div class="back-button" @mousedown="clicked">X</div>
+  <div class="back-button" @mousedown="clicked">
+    <XIcon />
+  </div>
 </template>
 
 <script>
+import XIcon from "./icon/XIcon.vue";
+
 export default {
   name: "BackButton",
+  components: { XIcon },
   methods: {
     clicked() {
       this.$emit("clicked");
@@ -18,8 +23,37 @@ export default {
 .back-button {
   position: absolute;
   color: white;
-  background-color: blueviolet;
+  background-color: black;
   border-radius: 8px;
-  padding: 1em 0.3em 1em 0.3em;
+  width: 30px;
+  height: 30px;
+  padding: 0.5em;
+  display: flex;
+  justify-content: center;
+  box-shadow: 0px 2px 8px gray;
+  cursor: pointer;
+  transition: background-color 0.15s ease-in-out;
+}
+
+:deep(path) {
+  transition: fill 0.15s ease-in-out;
+}
+
+:deep(.x-icon > path) {
+  fill: white;
+}
+
+.back-button:hover :deep(.x-icon > path) {
+  fill: black;
+}
+
+.back-button:hover {
+  background-color: white;
+}
+
+.back-button > img {
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
 }
 </style>
---
+++ src/components/icon/XIcon.vue
@@ -0,0 +1,26 @@
+<template>
+  <svg
+    viewBox="0 0 512 512"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    class="x-icon"
+  >
+    <path
+      :fill="this.color"
+      fill-rule="evenodd"
+      stroke="none"
+      d="M 399.282959 467 L 255.5 323.215271 L 111.715462 467 L 45 400.284729 L 188.784943 256.5 L 45 112.715271 L 111.715462 46 L 255.5 189.784729 L 399.282959 46 L 465.993622 112.715271 L 322.215454 256.5 L 465.993622 400.284729 Z"
+    />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: "XIcon",
+  props: {
+    color: { type: String, default: "#FFF" },
+  },
+};
+</script>
+
+<style scoped></style>
